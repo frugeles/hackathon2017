680d9c6a6218fbe5c4c9d27607b1256c70cd07b6
felipe-rugeles <felipe.rugeles@axa.fr>
2017-11-16 17:20:02 +0100

    calls and district ID data

--- code/maps.R
+++ code/maps.R
@@ -43,33 +43,50 @@ leaflet(options = leafletOptions(minZoom = 5, maxZoom = 14)) %>%
   )  
 
 
+
+
 #### Loading of 911 calls ####
-calls <- read.csv2(paste0(data_path,'call_data.csv'))
 
+# calls <- read.csv2(paste0(data_path,'call_data.csv'))
+# calls_clean <- calls[!(as.character(calls$Latitude)==''|as.character(calls$Latitude)==' '),]
+# calls_clean$Address <- as.character(calls_clean$Address)
+# calls_clean$Datetime <- as.Date(calls_clean$Datetime)
+# calls_clean$Longitude <- as.numeric(levels(calls_clean$Longitude))[calls_clean$Longitude]
+# calls_clean$Latitude <- as.numeric(levels(calls_clean$Latitude))[calls_clean$Latitude]
+# save.image(paste0(data_path,'calls.RData'))
 
-
-
+# Example
+load(paste0(data_path,'calls.RData'))
+calls <- calls_clean[1:1000,]
 spoints <- sp::SpatialPointsDataFrame(coords      = cbind(calls$Longitude, calls$Latitude),
-                                      bbox        = outlines.shape@bbox,
-                                      data        = building_permits,
+                                      bbox        = Seattle@bbox,
+                                      data        = calls,
                                       # We know that the CRS of seatl.bounds.shape is WGS84, since the data was downloaded from the same website, we assume that the crs 
                                       # is the same for both sources (WGS84 is also the most common crs). Also, based on the plot that follows, we know that it is OK.
-                                      proj4string = outlines.shape@proj4string, 
+                                      proj4string = Seattle@proj4string, 
                                       match.ID    = TRUE)
 
-outlines.shape.to.plot <- outlines.shape[sample(1000), ] # Take small sample to plot
-leaflet::leaflet(outlines.shape.to.plot) %>% 
+leaflet::leaflet(Seattle) %>% 
   leaflet::addTiles() %>%
-  leaflet::addPolygons(color = "red") %>%
-  leaflet::addCircles(lng = spoints$Longitude, lat = spoints$Latitude,
-                      weight = 1, 
-                      radius = 1)
-
-matched          <- sp::over(spoints, outlines.shape)
-spoints.fin@data <- cbind(spoints@data, matched$SHAPE_AREA)
-
-save(spoints.fin, file = "./Data/building_dat.RData")
+  leaflet::addPolygons(color = "red") %>% 
+  leaflet::addCircles(lng = spoints@data$Longitude, lat = spoints@data$Latitude)
 
 
 
+# Get the district ID for the calls
+calls <- calls_clean
+spoints <- sp::SpatialPointsDataFrame(coords      = cbind(calls$Longitude, calls$Latitude),
+                                      bbox        = Seattle@bbox,
+                                      data        = calls,
+                                      # We know that the CRS of seatl.bounds.shape is WGS84, since the data was downloaded from the same website, we assume that the crs 
+                                      # is the same for both sources (WGS84 is also the most common crs). Also, based on the plot that follows, we know that it is OK.
+                                      proj4string = Seattle@proj4string, 
+                                      match.ID    = TRUE)
+
+matched          <- sp::over(spoints, Seattle)
+calls['districtID'] <- matched$OBJECTID
+calls['district_name'] <- matched$S_HOOD
+calls['region_name'] <- matched$L_HOOD
+
+save(calls, file = paste0(data_path,'calls_district.RData'))
 
